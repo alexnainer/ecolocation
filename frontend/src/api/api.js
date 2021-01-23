e05ef280dec9a9788beeeea06181f7d207db3021
Alex Nainer <me@alexnainer.io>
2021-01-23 11:29:21 -0500

    Setting up Mongo + Mapbox map (#1)
    * connecting to mongo
    
    * added map

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -5,6 +5,11 @@ const url = process.env.REACT_APP_BACKEND_URL;
 axios.defaults.baseURL = `${url}/api/v1.0/ontario`;
 
 const api = {
+
+    getSomething() {
+        return axios.get(`/example`);
+    },
+
 };
 
 export default api;
---
+++ frontend/src/pages/map/MapPage.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import MapContainer from "./MapContainer";
+
+class MapPage extends Component {
+  render() {
+    return <MapContainer />;
+  }
+}
+
+export default MapPage;
