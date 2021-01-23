0f4423d55a4d562851c42cf222b2a1d3dcb80cbd
alexnainer <me@alexnainer.io>
2021-01-23 12:41:24 -0500

    added schemas

---
+++ backend/database/schemas.js
@@ -0,0 +1,58 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const locationSchema = new Schema(
+  {
+    name: String,
+    geoJson: {
+      type: String,
+      coordinates: [Number],
+    },
+    type: String,
+    address: String,
+    description: String,
+  },
+  { collection: "locations" }
+);
+
+const userSchema = new Schema(
+  {
+    name: String,
+    geoJson: {
+      type: String,
+      coordinates: [Number],
+    },
+    preferences: {
+      carType: String,
+      carpool: Boolean,
+      maxWalkDistance: Number,
+      maxCarDistance: Number,
+      maxBicycleDistance: Number,
+    },
+    results: {
+      distance: Number,
+      duration: Number,
+      transportationType: String,
+      routes: [{}],
+    },
+  },
+  { collection: "users" }
+);
+
+const sessionSchema = new Schema(
+  {
+    id: String,
+    users: [userSchema],
+    locationPreferences: [String],
+    results: {
+      cost: Number,
+    },
+  },
+  { collection: "sessions" }
+);
+
+const Location = mongoose.model("Location", locationSchema);
+const User = mongoose.model("User", userSchema);
+const Session = mongoose.model("Session", sessionSchema);
+
+export { Location, User, Session };
