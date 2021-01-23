4c5baccaa0d0eddf387514dc286821e436f68c42
Ryan <17rcp@queensu.ca>
2021-01-23 11:27:13 -0500

    beginning front end dev

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,9 +4,13 @@ import "./Header.css";
 class Header extends Component {
   render() {
     return (
-      <div className="d-flex justify-content-center pb-3">
-        <h1 className="header px-4">Alekx Health</h1>
-        {/* <FontAwesomeIcon icon={faStarOfLife} style={{ color: "red" }} /> */}
+      <div>
+        <div className="headerUpperline"></div>
+        <div className="d-flex justify-content-center pb-3 header-container">
+          <h1 className="header px-4">[environMEET logo]</h1>
+          {/* <FontAwesomeIcon icon={faStarOfLife} style={{ color: "red" }} /> */}
+        </div>
+        <div className="headerUnderline"></div>
       </div>
     );
   }
