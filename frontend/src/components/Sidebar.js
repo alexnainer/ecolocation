cd3f0f01f1348307417fa44723c3ea2d26b9fd02
Ryan <17rcp@queensu.ca>
2021-01-23 15:24:10 -0500

    front-end progression

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./Sidebar.css";
+import Card from 'react-bootstrap/Card'
 
 class Sidebar extends Component {
   render() {
@@ -7,6 +8,20 @@ class Sidebar extends Component {
       <div className="sidebar-container">
           <div className="meeting-header">
                 <h1 className="meeting-text">-- In your meeting --</h1>
+            </div>
+            <div className="list-container">
+                <Card.Header className="people-list oddCard" >
+                    <p className="personTab">Person 1</p>
+                </Card.Header>
+                <Card.Header className="people-list evenCard" >
+                    <p className="personTab">Person 2</p>
+                </Card.Header>
+                <Card.Header className="people-list oddCard" >
+                    <p className="personTab">Person 3</p>
+                </Card.Header>
+                <Card.Header className="people-list addCard">
+                    <p className="addToList">+ Click Here To Add a Person +</p>
+                </Card.Header>
             </div>
       </div>
     );
