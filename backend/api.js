88e3e977887cef43197536f523d22d599134d5fd
Aleks Jugovic <aleks.jugovic@gmail.com>
2021-01-23 01:35:37 -0500

    initlal api calls, calculator template

--- backend/api.js
+++ backend/api.js
@@ -1,5 +1,30 @@
 const axios = require("axios");
 
+const kingstonApiPrefix = "https://opendatakingston.cityofkingston.ca/api/records/1.0/search/?dataset=point-of-interest&q=&rows=10&facet=description&facet=name&refine.description="
+const kingstonApiSuffix = "&timezone=America%2FNew_York"
+
+const mapboxDirectionsApi = "https://api.mapbox.com/directions/v5/mapbox/"
+
+
+const mapBoxApiKey = process.env.MAPBOX_API_KEY;
+
+const getKingstonPointData = (options) => {
+  return axios.get(
+    '${kingstonApiPrefix}${options.location}${kingstonApiSuffix}'
+  );
+};
+
+
+const getMapBoxDirections = (options) => {
+  return axios.get(
+    '${mapboxDirectionsApi}${options.routing}/${options.coordinates}?alternatives=false&geometries=geojson&steps=true&access_token=${mapBoxApiKey}'
+  )
+}
+
+
+/*
+const axios = require("axios");
+
 const ontarioApi = "https://data.ontario.ca/api/3/action/datastore_search";
 const mapBoxApi = "https://api.mapbox.com/geocoding/v5/mapbox.places";
 const resourceIdPhu = "455fd63b-603d-4608-8216-7d8647f43350";
@@ -61,3 +86,4 @@ module.exports = {
   getGeocoding,
   getLtrData,
 };
+*/
---
+++ backend/calculator.js
@@ -0,0 +1,20 @@
+const gramsCO2PerMile = 404;
+
+
+const calculateCarbonEmission = (miles) => {
+    return gramsCO2PerMile * miles;
+}
+
+const calculateDistance = (origin, end) => {
+    //distance = api call to mapbox
+    //return distance
+}
+
+const driver = (peopleOptions, endPointOptions) => {
+    // carbon cost array initalize
+    // for every people location in people locations:
+    // for every endlocation in endlocations:
+    // distance = calculateDistance()
+    // carbonEmission = calculateCarbonEmission()
+}
+
