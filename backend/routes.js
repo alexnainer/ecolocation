db47ba9c9f968454d01dd0551f55777d7e0a205b
alexnainer <me@alexnainer.io>
2021-01-23 20:31:05 -0500

    added searchbar

--- backend/routes.js
+++ backend/routes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const api = require("./api");
 const sessions = require("./sessions");
 const users = require("./users");
+const driver = require("./calculator");
 
 router.get("/session/new", async (req, res) => {
   const result = await sessions.getSession();
@@ -15,6 +16,14 @@ router.get("/session/:sessionId", async (req, res) => {
   // const { query } = req;
   const { sessionId } = req.params;
 
+  const result = await sessions.getSession(sessionId);
+  res.send(result);
+});
+
+router.get("/session/:sessionId/calculate", async (req, res) => {
+  // const { query } = req;
+  const { sessionId } = req.params;
+  driver(sessionId);
   const result = await sessions.getSession(sessionId);
   res.send(result);
 });
