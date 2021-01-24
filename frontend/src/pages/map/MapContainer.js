4f542aedb400fb96322cd763f9394f56a6778b15
Aleks Jugovic <aleks.jugovic@gmail.com>
2021-01-23 21:00:37 -0500

    add outlines on map front-end

--- frontend/src/pages/map/MapContainer.js
+++ frontend/src/pages/map/MapContainer.js
@@ -3,9 +3,11 @@ import mapboxgl from "mapbox-gl";
 import "./MapContainer.css";
 import api from "../../api/api";
 import CircularProgress from "@material-ui/core/CircularProgress";
+import axios from "axios";
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_API_KEY;
 let map;
+
 
 class MapContainer extends Component {
   constructor(props) {
@@ -15,20 +17,66 @@ class MapContainer extends Component {
     };
   }
 
+
   async componentDidMount() {
+    var mapBounds = [
+      [-76.702106, 44.157578],
+      [-76.285721, 44.355278],
+    ];
     map = new mapboxgl.Map({
-      center: [-79.3832, 43.6532],
-      zoom: 7,
+      center: [-76.5, 44.233334],
+      zoom: 14,
       maxZoom: 18,
-      minZoom: 4,
+      minZoom: 12,
+      maxBounds: mapBounds,
       container: "map",
       style: "mapbox://styles/mapbox/streets-v11",
     });
 
     map.resize();
 
-    map.on("load", function () {
+    map.on("load", async function () {
       map.resize();
+      const result = await axios.get("https://api.mapbox.com/directions/v5/mapbox/walking/-76.502106%2C44.257578%3B-76.285721%2C44.355278?alternatives=false&overview=full&geometries=geojson&access_token=pk.eyJ1IjoiYWxleDlyIiwiYSI6ImNraDgybmpzaDE2ejEycm84NXpoOTJidjIifQ.3orrdNJLc-SghBYF8paqzQ");
+      console.log(result);
+      const json = result.data;
+      const data = json.routes[0];
+      const route = data.geometry.coordinates;
+      console.log(route);
+      const geojsont = {
+        type: 'Feature',
+        properties: {},
+        geometry: {
+          type: 'LineString',
+          coordinates: route
+        }
+      };
+      console.log("LENGTH: ", route.length);
+      map.addLayer({
+        id: 'route',
+        type: 'line',
+        source: {
+          type: 'geojson',
+          data: {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+              type: 'LineString',
+              coordinates: geojsont.geometry.coordinates,
+            }
+          }
+        },
+        layout: {
+          'line-join': 'round',
+          'line-cap': 'round'
+        },
+        paint: {
+          'line-color': '#3887be',
+          'line-width': 5,
+          'line-opacity': 0.75
+        }
+      });
+
     });
   }
 
