b4038b47cb2601ee90836f20c3527050bde8acb0
Ryan <17rcp@queensu.ca>
2021-01-23 20:32:02 -0500

    front end stuff

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,9 +1,21 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from 'react';
 import "./Sidebar.css";
 import Card from 'react-bootstrap/Card'
+import Checkbox from '@material-ui/core/Checkbox';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import Form from 'react-bootstrap/Form'
+import Select from 'react-select';
+
 
 class Sidebar extends Component {
   render() {
+    const distanceOptions = [
+        { value: '0', label: 'No Limit' },
+        { value: '1', label: '1' },
+        { value: '2', label: '2' },
+      ];
     return (
       <div className="sidebar-container">
             <div className="meeting-header">
@@ -58,6 +70,57 @@ class Sidebar extends Component {
             <div className="userUnderline"></div>
             <div className="userPreferences">
                 <p className="preferenceTextHeaders">Location</p>
+                <p className="preferenceTextHeaders">Method of Transportation</p>
+                <FormControl component="fieldset">
+                    <FormGroup aria-label="position" row>
+                        <FormControlLabel
+                            value="walking"
+                            control={<Checkbox color="primary" size="small" style={{ width: 25}}/>}
+                            label="Walking"
+                            labelPlacement="start"
+                            className="checkboxText"
+                        />
+                        <FormControlLabel
+                            value="walking"
+                            control={<Checkbox color="primary" size="small" style={{ width: 25}} />}
+                            label="Bicycle"
+                            width="1px"
+                            labelPlacement="start"
+                            className="checkboxText"
+                        />
+                        <FormControlLabel
+                            value="walking"
+                            control={<Checkbox color="primary" size="small" style={{ width: 25}}/>}
+                            label="Car"
+                            labelPlacement="start"
+                            className="checkboxText"
+                        />
+                        <FormControlLabel
+                            value="walking"
+                            control={<Checkbox color="primary" size="small" style={{ width: 25}}/>}
+                            label="Carpool"
+                            labelPlacement="start"
+                            className="checkboxText"
+                        />
+                    </FormGroup>
+                </FormControl>
+                <p className="preferenceTextHeaders">Max Travel Distance [km]</p>
+                <Form inline>
+                    <Form.Label className="my-1 mr-2" htmlFor="inlineFormCustomSelectPref">
+                        Walking
+                    </Form.Label>
+                    <Form.Control
+                        as="select"
+                        className="my-1 mr-sm-2 testFix"
+                        id="inlineFormCustomSelectPref"
+                        custom
+                    >
+                        <option value="0">No Limit</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </Form.Control>
+                </Form>
             </div>
 
                         {/* LOCATION PREFERENCES */}
