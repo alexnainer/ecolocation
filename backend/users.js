ce89c908ca74174a2904a50d9aee95eb7e305ab3
alexnainer <me@alexnainer.io>
2021-02-27 21:40:40 -0500

    reduce number of locations checked

--- backend/users.js
+++ backend/users.js
@@ -1,44 +1,44 @@
 const { User, Session } = require("./database/schemas");
 
-const log = require("debug")("sessions");
-const logError = require("debug")("sessions:error");
+const log = require("debug")("server");
+const logError = require("debug")("server:error");
 
 const options = { upsert: true, new: true };
 
 const createUser = async ({ name, sessionId }) => {
-  try {
-    console.log("name", name);
-    const user = await User.create({
-      name,
-      geoJson: {},
-      preferences: {
-        carType: "",
-        carpool: false,
-        maxWalkDistance: 0,
-        maxCarDistance: 0,
-        maxBicycleDistance: 0,
-      },
-    });
-    console.log("user", user);
-    await Session.findOneAndUpdate(
-      { id: sessionId },
-      { $push: { users: user._id } },
-      options
-    );
-  } catch (e) {
-    logError(e);
-  }
+    try {
+        console.log("name", name);
+        const user = await User.create({
+            name,
+            geoJson: {},
+            preferences: {
+                carType: "",
+                carpool: false,
+                maxWalkDistance: 0,
+                maxCarDistance: 0,
+                maxBicycleDistance: 0,
+            },
+        });
+        console.log("user", user);
+        await Session.findOneAndUpdate(
+            { id: sessionId },
+            { $push: { users: user._id } },
+            options
+        );
+    } catch (e) {
+        logError(e);
+    }
 };
 
 const updateUser = async (user) => {
-  try {
-    return await User.findOneAndUpdate({ _id: user._id }, user, options);
-  } catch (e) {
-    logError(e);
-  }
+    try {
+        return await User.findOneAndUpdate({ _id: user._id }, user, options);
+    } catch (e) {
+        logError(e);
+    }
 };
 
 module.exports = {
-  createUser,
-  updateUser,
+    createUser,
+    updateUser,
 };
--- frontend/src/pages/map/MapPage.js
+++ frontend/src/pages/map/MapPage.js
@@ -3,111 +3,125 @@ import MapContainer from "./MapContainer";
 import Header from "../../components/Header";
 import Sidebar from "../../components/Sidebar";
 import api from "../../api/api";
+import Snackbar from "@material-ui/core/Snackbar";
 import { withRouter } from "react-router";
 import Calculate from "../../components/Calculate";
 import CircularProgress from "@material-ui/core/CircularProgress";
 
 class MapPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loading: true,
-      mapLoading: false,
-      hasCalculated: false,
-      session: { results: {} },
-    };
-  }
-
-  async componentDidMount() {
-    const { sessionId } = this.props.match.params;
-
-    let response;
-    if (sessionId) {
-      response = await api.getSession(sessionId);
-    } else {
-      response = await api.getNewSession();
-      this.props.history.push(`/${response.data.id}`);
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: true,
+            mapLoading: false,
+            hasCalculated: false,
+            session: { results: {} },
+        };
     }
-    this.setState({ session: response.data, loading: false });
-  }
 
-  async handleAddPerson(name) {
-    const { sessionId } = this.props.match.params;
+    async componentDidMount() {
+        const { sessionId } = this.props.match.params;
 
-    // this.setState({ loading: true });
-    await api.postNewUser({ name, sessionId });
-    const response = await api.getSession(sessionId);
-    this.setState({ session: response.data });
-  }
+        let response;
+        if (sessionId) {
+            response = await api.getSession(sessionId);
+        } else {
+            response = await api.getNewSession();
+            this.props.history.push(`/${response.data.id}`);
+        }
+        this.setState({ session: response.data, loading: false });
+    }
 
-  async handleUpdatePerson(user) {
-    const { sessionId } = this.props.match.params;
-    // this.setState({ loading: true });
-    await api.postUpdateUser(user);
+    async handleAddPerson(name) {
+        const { sessionId } = this.props.match.params;
 
-    const { users } = this.state.session;
-    const index = users.findIndex((usr) => usr._id == user._id);
-    users[index] = user;
-    this.setState({
-      session: {
-        ...this.state.session,
-        users,
-      },
-    });
-  }
+        // this.setState({ loading: true });
+        await api.postNewUser({ name, sessionId });
+        const response = await api.getSession(sessionId);
+        this.setState({ session: response.data });
+    }
 
-  async handleUpdateSession(session) {
-    this.setState({ session });
-    await api.postUpdateSessionPreferences(
-      session.id,
-      session.locationPreferences
-    );
-  }
+    async handleUpdatePerson(user) {
+        const { sessionId } = this.props.match.params;
+        // this.setState({ loading: true });
+        await api.postUpdateUser(user);
 
-  handleCalculate = async () => {
-    this.setState({ mapLoading: true });
-    const { sessionId } = this.props.match.params;
-    await api.getCalculate(sessionId);
-    const response = await api.getSession(sessionId);
-    this.setState({
-      mapLoading: false,
-      session: response.data,
-      hasCalculated: true,
-    });
-  };
+        const { users } = this.state.session;
+        const index = users.findIndex((usr) => usr._id == user._id);
+        users[index] = user;
+        this.setState({
+            session: {
+                ...this.state.session,
+                users,
+            },
+        });
+    }
 
-  render() {
-    return (
-      <div>
-        <Header />
-        {!this.state.loading && (
-          <Fragment>
-            {this.state.mapLoading && (
-              <div className="loading position-absolute d-flex justify-content-center align-items-center">
-                <CircularProgress />
-              </div>
-            )}
-            <MapContainer
-              session={this.state.session}
-              loading={this.state.loading}
-            />
-            <Sidebar
-              loading={this.state.loading}
-              session={this.state.session}
-              addPerson={(name) => this.handleAddPerson(name)}
-              updatePerson={(user) => this.handleUpdatePerson(user)}
-              updateSession={(session) => this.handleUpdateSession(session)}
-            />
-          </Fragment>
-        )}
-        <Calculate
-          onClick={this.handleCalculate}
-          session={this.state.session}
-          hasCalculated={this.state.hasCalculated}
-        />
-      </div>
-    );
-  }
+    async handleUpdateSession(session) {
+        this.setState({ session });
+        await api.postUpdateSessionPreferences(
+            session.id,
+            session.locationPreferences
+        );
+    }
+
+    handleCalculate = async () => {
+        this.setState({ mapLoading: true });
+        const { sessionId } = this.props.match.params;
+        const calculateResponse = await api.getCalculate(sessionId);
+        console.log("calculateResponse", calculateResponse);
+        if (calculateResponse.status == 500) {
+            this.setState({ errorSnackbar: true });
+        }
+        const response = await api.getSession(sessionId);
+        this.setState({
+            mapLoading: false,
+            session: response.data,
+            hasCalculated: true,
+        });
+    };
+
+    render() {
+        return (
+            <div>
+                <Header />
+                {!this.state.loading && (
+                    <Fragment>
+                        {this.state.mapLoading && (
+                            <div className="loading position-absolute d-flex justify-content-center align-items-center">
+                                <CircularProgress />
+                            </div>
+                        )}
+                        <MapContainer
+                            session={this.state.session}
+                            loading={this.state.loading}
+                        />
+                        <Sidebar
+                            loading={this.state.loading}
+                            session={this.state.session}
+                            addPerson={(name) => this.handleAddPerson(name)}
+                            updatePerson={(user) =>
+                                this.handleUpdatePerson(user)
+                            }
+                            updateSession={(session) =>
+                                this.handleUpdateSession(session)
+                            }
+                        />
+                    </Fragment>
+                )}
+                <Calculate
+                    onClick={this.handleCalculate}
+                    session={this.state.session}
+                    hasCalculated={this.state.hasCalculated}
+                />
+                <Snackbar
+                    open={this.state.errorSnackbar}
+                    onClose={() => this.setState({ errorSnackbar: false })}
+                    message="An error has occurred"
+                />
+            </div>
+        );
+    }
 }
 
 export default withRouter(MapPage);
