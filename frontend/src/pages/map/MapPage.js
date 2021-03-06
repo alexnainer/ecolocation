8d485e48e4187341f0e3b7302c4450699c3e3b47
alexnainer <me@alexnainer.io>
2021-03-06 01:06:04 -0500

    messed up but kinda working

--- frontend/src/pages/map/MapPage.js
+++ frontend/src/pages/map/MapPage.js
@@ -6,6 +6,7 @@ import api from "../../api/api";
 import { withRouter } from "react-router";
 import Calculate from "../../components/Calculate";
 import CircularProgress from "@material-ui/core/CircularProgress";
+import SnackbarAlert from "../../components/SnackbarAlert";
 
 class MapPage extends Component {
   constructor(props) {
@@ -64,16 +65,55 @@ class MapPage extends Component {
     );
   }
 
+  isFieldsValid = () => {
+    for (const user of this.state.session.users) {
+      console.log("user", user);
+      if (!user.location?.geoJson?.coordinates?.length) return false;
+
+      const {
+        maxWalkDistance,
+        maxCarDistance,
+        maxBicycleDistance,
+      } = user.preferences;
+
+      if (!(maxWalkDistance || maxCarDistance || maxBicycleDistance))
+        return false;
+    }
+    return true;
+  };
+
+  isLocationPreferencesValid = () => {
+    const {
+      cafe,
+      outdoors,
+      government,
+      restaurant,
+      hotel,
+      publicBuilding,
+    } = this.state.session.locationPreferences;
+    return (
+      cafe || outdoors || government || restaurant || hotel || publicBuilding
+    );
+  };
+
   handleCalculate = async () => {
-    this.setState({ mapLoading: true });
-    const { sessionId } = this.props.match.params;
-    await api.getCalculate(sessionId);
-    const response = await api.getSession(sessionId);
-    this.setState({
-      mapLoading: false,
-      session: response.data,
-      hasCalculated: true,
-    });
+    if (!this.state.session.users.length) {
+      this.setState({ missingUsersError: true });
+    } else if (!this.isFieldsValid()) {
+      this.setState({ validFieldsError: true });
+    } else if (!this.isLocationPreferencesValid()) {
+      this.setState({ locationPreferencesError: true });
+    } else {
+      this.setState({ mapLoading: true });
+      const { sessionId } = this.props.match.params;
+      await api.getCalculate(sessionId);
+      const response = await api.getSession(sessionId);
+      this.setState({
+        mapLoading: false,
+        session: response.data,
+        hasCalculated: true,
+      });
+    }
   };
 
   render() {
@@ -104,6 +144,24 @@ class MapPage extends Component {
           onClick={this.handleCalculate}
           session={this.state.session}
           hasCalculated={this.state.hasCalculated}
+        />
+        <SnackbarAlert
+          open={this.state.validFieldsError}
+          onClose={() => this.setState({ validFieldsError: false })}
+          severity="error"
+          message="Missing user fields"
+        />
+        <SnackbarAlert
+          open={this.state.missingUsersError}
+          onClose={() => this.setState({ missingUsersError: false })}
+          severity="error"
+          message="At least 1 user is required"
+        />
+        <SnackbarAlert
+          open={this.state.locationPreferencesError}
+          onClose={() => this.setState({ locationPreferencesError: false })}
+          severity="error"
+          message="At least 1 location preference is required"
         />
       </div>
     );
